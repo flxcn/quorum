e934ecf88764c444bb97da61372a2c16e3343a8d
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-05 01:08:58 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    <link rel="icon" href="assets/images/icon.png">
+
+    <title>Dialogue</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/css/index.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+    
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead mb-auto">
+            <div class="inner">
+                <h3 class="masthead-brand"><b>Quorum</b></h3>
+                <nav class="nav nav-masthead justify-content-center">
+                    <a class="nav-link active" href="#">Home</a>
+                    <a class="nav-link" href="moderator/sign-in.php">For Staff</a>
+                    <a class="nav-link" href="about.php">About</a>
+                    <a class="nav-link" href="mailto:felixchen@college.harvard.edu">Contact</a>
+                </nav>
+            </div>
+        </header>
+
+        <main role="main" class="inner cover">
+            <h1 class="cover-heading"><b>Welcome!</b></h1>
+            <p class="cover-description lead py-1"><i><b>Quorum</b></i> is an online voting platform specifically designed with parlimentary procedure in mind.</p>
+            <p class="lead">
+            <a href="delegate/sign-in.php" class="btn btn-lg btn-secondary">Open session!</a>
+            </p>
+        </main>
+
+        <footer class="mastfoot mt-auto">
+            <div class="inner">
+                <p>Created by <a href="https://felixchen.com" target="_blank">Felix Chen</a></p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+</body>
+</html>
