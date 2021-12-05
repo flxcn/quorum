a791d9ea13b07dc53827894725255cc2eeb046e6
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-05 00:08:44 -0500

    Create DatabaseConnection.php

---
+++ classes/DatabaseConnection.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+ini_set('log_errors',1);
+error_reporting(E_ALL);
+
+define('DB_HOST','localhost');
+define('DB_NAME','quorum');
+define('DB_CHARSET','utf8mb4');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+define("MAX_YEA_DELEGATE_BALLOTS",7);
+define("MAX_YEA_CAUCUS_BALLOTS",7);
+
+date_default_timezone_set('America/New_York');
+
+class DatabaseConnection
+{
+    protected static $instance;
+    protected $pdo;
+
+    public function __construct() {
+			$options = [
+					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+					PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+      $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET;
+      $this->pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
+    }
+
+    public static function instance()
+    {
+        if (self::$instance === null)
+        {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->pdo, $method), $args);
+    }
+
+    public function getPDO()
+	{
+		return $this->pdo;
+	}
+}
+?>
