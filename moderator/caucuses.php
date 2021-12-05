66b53724e73209473f8b596368902aef27c4c786
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-05 13:20:30 -0500

    Create caucuses.php

---
+++ moderator/caucuses.php
@@ -0,0 +1,114 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["moderator_signed_in"]) || $_SESSION["moderator_signed_in"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
+
+require_once "../classes/ModeratorDashboard.php";
+$obj = new ModeratorDashboard();
+$caucuses = $obj->getCaucuses(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Delegates</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/dashboard.css" rel="stylesheet">
+
+    <link rel="icon" href="../assets/images/icon.png">
+</head>
+
+<body class="bg-light">
+    <header>
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"><img src="../assets/images/icon.png" alt="" width="32" height="32"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample02">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="votes.php">Votes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="delegates.php">Delegates </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="caucuses.php">Caucuses <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="sign-out.php">Sign out</a>
+                    </li>
+                </ul>
+        </div>
+        </nav>
+    </header>
+    
+    <div class="container">
+        <div class="pt-4 text-center">
+            <h2>Constitutional Convention</h2>
+            <p class="lead">Roster of Caucuses</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 order-md-1">
+                <?php if($caucuses):?>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Caucus Title</th>
+                            <th scope="col">Full Description</th>
+                            <!-- <th scope="col">Members</th> -->
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($caucuses as $caucus):?>
+                            <tr>
+                                <th scope="row"><?php echo $caucus["caucus_id"]; ?></th>
+                                <td><?php echo $caucus["title"]; ?></td>
+                                <td><?php echo $caucus["description"]; ?></td>
+                                <!-- <td><?php // echo $obj->getCaucusMembers["caucus_id"]; ?></td> -->
+                                <!-- <td><a href="caucus-read.php?caucus_id=<?php // echo $caucus['caucus_id']; ?>">View Members</a></td> -->
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                <?php else: ?>
+                    <p class="text-center"><i>No delegates added yet.</i></p>
+                <?php endif;?>
+            </div>
+        </div>
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <p class="mb-1">© 2021 Felix Chen</p>
+        </footer>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Custom JS for this page -->
+    <!-- Define JS variables to hold PHP session variables -->
+
+    <!-- Script for JQuery Functions -->
+    <script src="../assets/js/moderator-dashboard.js"></script>
+</body>
+</html>
