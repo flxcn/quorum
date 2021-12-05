3276e272b6abc312e362a01252c9f93b990dcd3d
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-05 13:23:59 -0500

    Create ModeratorDashboard.php

---
+++ classes/ModeratorDashboard.php
@@ -0,0 +1,53 @@
+<?php
+require_once "DatabaseConnection.php";
+
+class ModeratorDashboard {
+        protected $pdo = null;
+
+        public function __construct() {
+                $this->pdo = DatabaseConnection::instance();
+	}
+
+        public function getDelegates() {
+                $sql =
+                        "SELECT * FROM delegates
+                        ORDER BY first_name ASC";
+                $stmt = $this->pdo->prepare($sql);
+                $status = $stmt->execute();
+                if(!$status) {
+                        return null;
+                }
+                else {
+                        $delegates = $stmt->fetchAll();
+                        return $delegates;
+                }
+        }
+
+        public function getCaucuses() {
+        $sql =
+                "SELECT * FROM caucuses";
+        $stmt = $this->pdo->prepare($sql);
+                $status = $stmt->execute();
+                if(!$status) {
+                        return null;
+                }
+                else {
+                        $caucuses = $stmt->fetchAll();
+                        return $caucuses;
+                }
+        }
+
+        public function getVotes() {
+                $sql = "SELECT * FROM votes";
+                $stmt = $this->pdo->prepare($sql);
+                $status = $stmt->execute();
+                if(!$status) {
+                        return null;
+                }
+                else {
+                        $votes = $stmt->fetchAll();
+                        return $votes;
+                }
+        }
+}
+?>
