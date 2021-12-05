d7d4c7290c883ebc4c2e4360754331bb03ce0451
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-05 00:07:57 -0500

    Create Ballot.php

---
+++ classes/Ballot.php
@@ -0,0 +1,75 @@
+<?php
+require_once "DatabaseConnection.php";
+
+class Ballot {
+    protected $pdo = null;
+    private $delegate_id;
+    private $vote_id;
+    private $decision;
+    private $created_on;
+
+    public function __construct() {
+        $this->pdo = DatabaseConnection::instance();
+	}
+
+    // getters and setters
+
+    public function setDelegateId($delegate_id) {
+        $this->delegate_id = $delegate_id;
+    }
+
+    public function getDelegateId() {
+        return $this->delegate_id;
+    }
+
+    public function setVoteId($vote_id) {
+        $this->vote_id = $vote_id;
+    }
+
+    public function getVoteId() {
+        return $this->vote_id;
+    }
+
+    public function setDecision($decision) {
+        $this->decision = $decision;
+    }
+    public function getDecision() {
+        return $this->decision;
+    }
+
+    public function addBallot() {
+        $sql = "INSERT INTO ballots (vote_id, delegate_id, decision)
+			VALUES (:vote_id, :delegate_id, :decision)";
+		$stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(
+			[
+				'vote_id' => $this->vote_id,
+                'delegate_id' => $this->delegate_id,
+                'decision' => $this->decision
+			]);
+
+		return $status;
+    }
+
+    public function countRemainingYeaVotes() {
+        $sql =
+            "SELECT 
+                COUNT(*) 
+            FROM 
+                ballots
+            WHERE decision = true
+            AND delegate_id = :delegate_id";
+        
+        $stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(['delegate_id' => $this->delegate_id]);
+        $count = $stmt->fetchColumn();
+        return MAX_YEA_DELEGATE_BALLOTS - $count;
+    }
+
+    public function checkBallotExists($vote_id, $delegate_id): bool {
+        $stmt = $this->pdo->prepare("SELECT 1 FROM ballots WHERE vote_id = :vote_id AND delegate_id = :delegate_id");
+        $stmt->execute(['vote_id' => $vote_id, 'delegate_id' => $delegate_id]);
+        return (bool)$stmt->fetch();
+    }
+}
+?>
