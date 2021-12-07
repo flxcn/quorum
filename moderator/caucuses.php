48c45cefb55e7ec952ad4655d83f491ede36b80c
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-06 22:37:50 -0500

    Update caucuses.php

--- moderator/caucuses.php
+++ moderator/caucuses.php
@@ -62,9 +62,13 @@ $caucuses = $obj->getCaucuses();
     </header>
     
     <div class="container">
-        <div class="pt-4 text-center">
-            <h2>Constitutional Convention</h2>
-            <p class="lead">Roster of Caucuses</p>
+        <div class="pt-4 d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Roster of Caucuses</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group me-2">
+                    <a href="caucus-create.php" class="btn btn-sm btn-outline-success">Add New</a>
+                </div>
+            </div>
         </div>
 
         <div class="row">
