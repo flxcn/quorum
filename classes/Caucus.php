f051cd47d36af76d1e873f5e67c7e02993ed32b0
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-06 22:37:24 -0500

    Create Caucus.php

---
+++ classes/Caucus.php
@@ -0,0 +1,79 @@
+<?php
+require_once "DatabaseConnection.php";
+
+class Caucus {
+    protected $pdo = null;
+    private $caucus_id;
+    private $title;
+    private $description;
+
+    public function __construct() {
+        $this->pdo = DatabaseConnection::instance();
+	}
+
+    // getters and setters
+
+    public function setCaucusId($caucus_id) {
+        $this->caucus_id = $caucus_id;
+    }
+
+    public function getCaucusId() {
+        return $this->caucus_id;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function addCaucus() {
+        $sql = "INSERT INTO caucuses (title, description)
+			VALUES (:title, :description)";
+		$stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(
+			[
+				'title' => $this->title,
+                'description' => $this->description,
+			]);
+
+		return $status;
+    }
+
+    public function updateCaucus() {
+        $sql = 
+            "UPDATE delegates 
+            SET name = :name,
+                email = :email,
+            WHERE delegate_id = :delegate_id";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute(
+            [
+                'name' => $this->name,
+                'email' => $this->email,
+                'delegate_id' => $this->delegate_id
+            ]);
+
+        return $status;
+    }
+
+    public function markPresent() {
+        $sql = 
+            "UPDATE delegates 
+            SET is_present = 1
+            WHERE delegate_id = :delegate_id";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute(['delegate_id' => $this->delegate_id]);
+        return $status;
+    }
+
+}
+?>
