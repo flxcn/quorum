dc0b9a257af1eb8dfad593de7c5a2d689400f3c2
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-07 19:16:38 -0500

    Update dashboard.php

--- delegate/dashboard.php
+++ delegate/dashboard.php
@@ -122,13 +122,55 @@ $obj = new DelegateDashboard($_SESSION["delegate_id"]);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
     <!-- Custom JS for this page -->
-    <!-- Define JS variables to hold PHP session variables -->
-    <script type="text/javascript">
-        var delegate_id  = <?php echo $_SESSION['delegate_id']; ?>;
-    </script>
-
     <!-- Script for JQuery Functions -->
-    <script src="../assets/js/dashboard.js"></script>
+    <!-- <script src="../assets/js/dashboard.js"></script> -->
+    <script>
+        $(document).ready(function(){
 
+        getOngoingVotes();
+
+        setInterval(function(){
+            getOngoingVotes();
+        }, 5000);
+
+        function getOngoingVotes()
+        {
+            $.ajax({
+                url:"get-active-votes.php",
+                method:"POST",
+                data: {},
+                success:function(data){
+                    var html = '';
+                    var count = Object.keys(data).length;
+                    $.each(data, function(key, value){
+
+                        html += '<div class="card bg-white">';
+                        html += '    <div class="card-header bg-white text-black"><b>'+ value.title +'</b></div>';
+                        html += '    <div class="card-body overflow-auto" id="messagesArea">';
+                        html += '        <p>Proposed by: <b>' + value.sponsor + ' (' + value.caucus + ')</b></p>';
+                        html += '        <p>'+ value.description + '</p>';
+                        html += '        <p><a target="_blank" href="'+ value.link + '">See text of amendment</a></p>';
+                        html += '        <p class="card-text"><i>Remember, amendments require 3/4 of delegates to pass.</i></p>';
+                        html += '    </div>';
+                        html += '    <div class="card-footer overflow-auto bg-white">';
+                        if(value.is_open_for_delegates) {
+                            html += '        <a class="btn btn-success" href="delegate-vote.php?vote_id=' + value.vote_id + '">Vote as a Delegate</a>';
+                        }
+                        if(value.is_open_for_caucuses) {
+                            html += '        <a class="btn btn-primary" href="caucus-vote.php?vote_id=' + value.vote_id + '">Vote as a Caucus</a>';
+                        }
+                        html += '    </div>';
+                        html += '</div>';
+                    });
+                    $('#ongoingVotesCount').html(count);
+                    $('#ongoingVotes').html(html);
+                },
+                error: function (request, status, error) {
+                    alert(request.responseText);
+                }     
+            })
+        }
+        }); 
+    </script>
 </body>
 </html>
