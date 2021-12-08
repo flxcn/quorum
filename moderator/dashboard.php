f818e9e8cc9a55fa7b56ddddbf0dc965b783a8b2
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-08 01:26:53 -0500

    Update dashboard.php

--- moderator/dashboard.php
+++ moderator/dashboard.php
@@ -35,7 +35,7 @@
                         <a class="nav-link" href="delegates.php">Delegates</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="delegates.php">Caucuses</a>
+                        <a class="nav-link" href="caucuses.php">Caucuses</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-auto">
