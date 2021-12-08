255ab0e59a7bbc9d31dd23104475364e370e76a0
Felix <30987125+flxcn@users.noreply.github.com>
2021-12-07 22:26:02 -0500

    Update ModeratorDashboard.php

--- classes/ModeratorDashboard.php
+++ classes/ModeratorDashboard.php
@@ -2,54 +2,56 @@
 require_once "DatabaseConnection.php";
 
 class ModeratorDashboard {
-        protected $pdo = null;
+    protected $pdo = null;
 
-        public function __construct() {
-                $this->pdo = DatabaseConnection::instance();
-	}
+    public function __construct() {
+        $this->pdo = DatabaseConnection::instance();
+    }
 
-        public function getDelegates() {
-                $sql =
-                        "SELECT * FROM delegates
-                        INNER JOIN caucuses 
-                        ON delegates.caucus_id = caucuses.caucus_id
-                        ORDER BY first_name ASC";
-                $stmt = $this->pdo->prepare($sql);
-                $status = $stmt->execute();
-                if(!$status) {
-                        return null;
-                }
-                else {
-                        $delegates = $stmt->fetchAll();
-                        return $delegates;
-                }
-        }
-
-        public function getCaucuses() {
+    // Get an array of all Delegates
+    public function getDelegates() {
         $sql =
-                "SELECT * FROM caucuses";
+            "SELECT * FROM delegates
+            INNER JOIN caucuses 
+            ON delegates.caucus_id = caucuses.caucus_id
+            ORDER BY first_name ASC";
         $stmt = $this->pdo->prepare($sql);
-                $status = $stmt->execute();
-                if(!$status) {
-                        return null;
-                }
-                else {
-                        $caucuses = $stmt->fetchAll();
-                        return $caucuses;
-                }
+        $status = $stmt->execute();
+        if(!$status) {
+            return null;
         }
+        else {
+            $delegates = $stmt->fetchAll();
+            return $delegates;
+        }
+    }
 
-        public function getVotes() {
-                $sql = "SELECT * FROM votes";
-                $stmt = $this->pdo->prepare($sql);
-                $status = $stmt->execute();
-                if(!$status) {
-                        return null;
-                }
-                else {
-                        $votes = $stmt->fetchAll();
-                        return $votes;
-                }
+    // Get an array of all Caucuses
+    public function getCaucuses() {
+        $sql = "SELECT * FROM caucuses";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute();
+        if(!$status) {
+            return null;
         }
+        else {
+            $caucuses = $stmt->fetchAll();
+            return $caucuses;
+        }
+    }
+
+    // Get an array of all Caucuses
+    public function getVotes() {
+        $sql = "SELECT * FROM votes";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute();
+        if(!$status) {
+            return null;
+        }
+        else {
+            $votes = $stmt->fetchAll();
+            return $votes;
+        }
+    }
 }
 ?>
